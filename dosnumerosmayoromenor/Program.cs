82e11a021ef5baa806673c1255324cc40af00663
Umblum12 <68798247+Umblum12@users.noreply.github.com>
2022-03-25 03:25:37 -0700

    Agregar archivos de proyecto.

---
+++ dosnumerosmayoromenor/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace dosnumerosmayoromenor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float num;
+            float num2;
+            Console.WriteLine("Introdusca el primer numero: ");
+            num = float.Parse(Console.ReadLine());
+            Console.WriteLine("Introdusca el primer numero: ");
+            num2 = float.Parse(Console.ReadLine());
+            if (num > num2)
+            {
+                Console.WriteLine("El numero mayor es: " + num);
+            }
+            else if (num == num2)
+            {
+                Console.WriteLine("Estos numeros son iguales");
+            }
+            else
+            {
+                Console.WriteLine("El numero mayor es: " + num2);
+            }
+            {
+
+            }
+        }
+    }
+}
